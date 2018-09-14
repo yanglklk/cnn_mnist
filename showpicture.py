7331fc3551446da5b20265e68f6d0946d6b2d8f1
yanglklk <1085960411@qq.com>
2018-09-14 21:33:51 +0800

    right_path

--- showpicture.py
+++ showpicture.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import struct
-
-path = r'D:\_____\Works\PyProject\tensortest\code\MNIST_data'
-
+#this file need publish ,so use my path
+# add function1
+path1 =r'D:\_____\Works\PyProject\tensortest\image_cv\ori_data'
+path2='D:\_____\Works\PyProject\tensortest\image_cv\data'
 def read_image(filename):
     f = open(filename, 'rb')
 
@@ -23,10 +24,10 @@ def read_image(filename):
                 image.putpixel((y, x), int(struct.unpack_from('>B', buf, index)[0]))
                 index += struct.calcsize('>B')
 
-        # print('save ' + str(i) + 'image')
-        image.save(path+r'\train' +'\\'+ str(i) + '.png')
-
-
+        #save train or test
+        #image.save(path2+r'\train'+'\\'+ str(i) + '.png')
+        image.save(path2 + r'\test' + '\\' + str(i) + '.png')
+        
 def read_label(filename, saveFilename):
     f = open(filename, 'rb')
     index = 0
@@ -56,6 +57,7 @@ def read_label(filename, saveFilename):
 
 
 if __name__ == '__main__':
-
-    read_image(path+r'\train-images.idx3-ubyte')
-    read_label(path+r'\train-labels.idx1-ubyte',path+ r'\train_label.txt')
+    read_image(path1 + r'\t10k-images.idx3-ubyte')
+    read_label(path1 + r'\t10k-labels.idx1-ubyte', path2 + r'\train_label.txt')
+    read_image(path1 + r'\train-images.idx3-ubyte')
+    read_label(path1 + r'\train-labels.idx1-ubyte', path2 + r'\train_label.txt')
